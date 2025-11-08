8325bd299fc1e996b693ef63fb7d18128238b931
some1 <>
2025-11-08 16:36:53 +1100

    Started working on config parsing and potential config options. Commiting early due to a power outage.

---
+++ src/config.py
@@ -0,0 +1,37 @@
+import os
+import tomllib
+from typing import Any
+import tomli_w
+
+def get_config() -> dict[str, Any]:  # pyright: ignore[reportExplicitAny]
+    if os.name == "nt":
+        if (appdata := os.environ.get('LOCALAPPDATA')): # Walrus is good >:3
+            CONFIG_DIR =  os.path.join(appdata, "g4music-discord-rpc/")
+        else:
+            CONFIG_DIR = os.path.join(os.curdir, "config/")
+    else:
+        if (config_dir := os.environ.get('XDG_CONFIG_HOME')):
+            CONFIG_DIR = os.path.join(config_dir, "g4music-discord-rpc/")
+        else:
+            CONFIG_DIR = os.path.join(os.environ.get("HOME", ""), ".config", "g4music-discord-rpc/")
+
+    CONFIG_FILE = os.path.join(CONFIG_DIR, "config.toml")
+
+    DEFAULT_CONFIG = {
+        "appid": 1436573238636576891,
+        "idle-detect": False,
+        "details": "",
+        "show-queue-size": True,
+        "image-hover": "Listening to {song}",
+        "icon": True,
+        "cover-art": True,
+        "state": "{song} / {album} - {artist}"
+    }
+
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+    if not os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "wb") as f:
+            tomli_w.dump(DEFAULT_CONFIG, f)
+
+    with open(CONFIG_FILE, "rb") as f:
+        return tomllib.load(f)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-import discordrpc
+from discordrpc import RPC
+from .config import get_config
 
-app = discordrpc.RPC(
-    app_id=1436573238636576891,
+config = get_config()
+
+app = RPC(
+    app_id=config.get("appid") or 1436573238636576891,
     exit_if_discord_close=False,
     exit_on_disconnect=False
 )
+
+app.run()
+
+# Use dbus to query gapless.
+# Will concat a property when called.
+command = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.com.github.neithern.g4music /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string: "
